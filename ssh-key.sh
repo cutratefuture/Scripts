51cfee617aba25c3b1a487e41080087c2c7d63d6
cutratefuture <christian@cutratefuture.com>
2021-04-26 16:44:33 -0700

    out with the old

--- ssh-key.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_ed25519
-cat .ssh/id_ed25519.pub 
--- sudouser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# USERNAME = NAME
-USERNAME = bcat
-
-sudo adduser USERNAME
-usermod -aG sudo USERNAME
