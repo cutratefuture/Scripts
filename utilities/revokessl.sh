224299aac9a9db01c61854a30bcb3e318c0107ae
cutratefuture <christian@cutratefuture.com>
2021-04-26 18:15:36 -0700

    add proto revoke script

---
+++ utilities/revokessl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# certbot revoke script
+# switch to root to output the contents of dir
+echo "list of SSL certs installed:"
+sudo ls -la /etc/letsencrypt/archive/
+read -p 'Enter domain for SSL revoke' domain
+#cat pem files in dir
+echo "typically the fullchain.pem file will fully revoke the SSL and delete the entry"
+echo "fullchain1.pem or fullchain3.pem will be enough"
+sudo ls -la /etc/letsencrypt/archive/$domain/
+read -p 'Enter pem filename' fullchain
+# revoke
+certbot revoke --cert-path /etc/letsencrypt/archive/$domain/$fullchain.pem
