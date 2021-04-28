575e655b59e065d0caac6abbccc35334c5833a7a
cutratefuture <christian@cutratefuture.com>
2021-04-28 16:57:59 -0700

    - update user script - added docker

---
+++ utilities/addsudo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+red="\e[0;91m"
+green="\e[0;92m"
+uline="\e[4m"
+reset="\e[0m"
+
+# Am i Root user?
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username ${red}exists${reset}!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd  -aG -m -p "$pass" sudo "$username" 
+		[ $? -eq 0 ] && echo "${green}User has been added to system!${reset}" || echo "${red}Failed to add a user!${reset}"
+	fi
+else
+	echo "${red}Only root may add a user to the system.${reset}"
+	exit 2
+fi
