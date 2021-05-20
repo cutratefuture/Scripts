cd06792b255126a91536ee97e32fad6a33c87e86
cutratefuture <christian@cutratefuture.com>
2021-05-20 18:58:23 +0000

    update letsencrupt scipt

--- utilities/letsencrypt_nginx.sh
+++ utilities/letsencrypt_nginx.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #install certbot for nginx
-sudo apt install certbot python3-certbot-nginx -y
-
+if ! command -v certbot &> /dev/null
+then
+    sudo apt install certbot python3-certbot-nginx -y
+    echo "Certbot not found, installing..."
+    exit
+fi
 # prompt and run
 read -p  "Enter domain:" d
 sudo certbot --nginx -d $d -d www.$d
